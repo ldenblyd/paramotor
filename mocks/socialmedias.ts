278625a0c8166319248b08bf8840276fbd64af8b
Loan Denblyden <l.denblyden@gmail.com>
2021-09-29 01:31:50 +0200

    feat: add social medias links in mock

---
+++ mocks/socialmedias.ts
@@ -0,0 +1,6 @@
+export default {
+  instagram: "https://www.instagram.com/youfly.io/",
+  facebook: "https://www.facebook.com/youfly.io",
+  youtube: "https://www.youtube.com/channel/UCnxpG0F1RvfcGuY5_Zs3YBg",
+  twitter: "https://twitter.com/youfly_io",
+};
