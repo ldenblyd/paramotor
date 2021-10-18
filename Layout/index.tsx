9cc98f8da379e0767993066411ee8e4405af4cd4
Loan Denblyden <l.denblyden@gmail.com>
2021-10-18 22:30:39 +0200

    feat: google verification

--- Layout/index.tsx
+++ Layout/index.tsx
@@ -6,6 +6,7 @@ const Layout: React.FC = ({ children }) => {
       <Head>
         <title>YOUFLY</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="hrv8W8apHCN2Kj2ZgyoPibs_KEWsRg0CucecwdKa9zo" />
       </Head>
       {children}
     </div>
